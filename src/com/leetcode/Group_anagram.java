5c433684559e86cee76e449134fb7996a867b0c5
pratham2609 <pratham1055.be21@chitkara.edu.in>
2024-02-07 00:36:08 +0530

    questions

---
+++ src/com/leetcode/Group_anagram.java
@@ -0,0 +1,26 @@
+package com.leetcode;
+import java.util.*;
+
+public class Group_anagram {
+    public static void main(String[] args) {
+        String[] strs = {"eat","tea","tan","ate","nat","bat"};
+        System.out.println(groupAnagrams(strs));
+    }
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+        // iterate through each string
+        for(String s:strs){
+            char[] count = new char[26];
+            for(char ch : s.toCharArray()){
+                // - 'a' so that the array doesnot go out of bound
+                count[ch - 'a']++;
+            }
+            String str = new String(count);
+            if(!map.containsKey(str)) {
+                map.put(str, new ArrayList<>());
+            }
+            map.get(str).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
